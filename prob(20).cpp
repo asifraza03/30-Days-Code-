792dd046abac62065da188be931245801779c2e0
Asif Raza <90905808+asifraza03@users.noreply.github.com>
2023-08-09 10:30:38 +0530

    Update prob(20).cpp

--- prob(20).cpp
+++ prob(20).cpp
@@ -1,27 +1,59 @@
 #include<iostream>
 using namespace std;
-int index(int array[],int size)
+int secondLargest(int array[],int size)
 {
     int max=INT_MIN;
-    int count = 0;
+    int secmax=INT_MIN;
     for(int i=0;i<size;i++)
     {
-        if(array[i]> max)
+        if(array[i]>max)
         {
-            max=array[i];
-            count=i;
+            max = array[i];
         }
-
     }
-    return count;
+    for(int i=0;i<size;i++)
+    {
+        if( array[i]>secmax && array[i]!=max)
+        {
+            secmax = array[i];
+        }
+    }
+    return secmax;
 }
 int main()
 {
-    int arr[]={1,56,23,77,42,19,5,17};
-    int first = index(arr,8);
-    arr[first] = -1;
-    int second = index(arr,8);
-    cout<<arr[second]<<endl;
+    int arr[]={12,11,12,12,11,5,6};
+    cout<<secondLargest(arr,7);
     return 0;
 
 }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int index(int array[],int size)
+// {
+//     int max=INT_MIN;
+//     int count = 0;
+//     for(int i=0;i<size;i++)
+//     {
+//         if(array[i]> max)
+//         {
+//             max=array[i];
+//             count=i;
+//         }
+
+//     }
+//     return count;
+// }
+// int main()
+// {
+//     int arr[]={1,56,23,77,42,19,5,17};
+//     int first = index(arr,8);
+//     arr[first] = -1;
+//     int second = index(arr,8);
+//     cout<<arr[second]<<endl;
+//     return 0;
+
+// }
