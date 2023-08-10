3d978a26a960974dfb62f707c701eff925338451
Asif Raza <90905808+asifraza03@users.noreply.github.com>
2023-08-10 10:35:32 +0530

    Create prob(25).cpp

---
+++ prob(25).cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void edit(vector<int> &v) {
+    int left = 0;
+    int right = v.size() - 1;
+    while (left < right) {
+        if (v[left] % 2 == 1 && v[right] % 2 == 0) {
+            // swap(v[left], v[right]);
+ int temp = v[left];
+            v[left] = v[right];
+            v[right] = temp;
+            left++;
+            right--;
+        }
+        if (v[left] % 2 == 0) {
+            left++;
+        }
+        if (v[right] % 2 == 1) {
+            right--;
+        }
+    }
+    return;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < n; i++) {
+        int elem;
+        cin >> elem;
+        v.push_back(elem);
+    }
+    edit(v);
+    for (int i = 0; i < n; i++) {
+        cout << v[i] << " ";
+    }
+    return 0;
+}
