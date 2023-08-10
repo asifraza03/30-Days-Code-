af86ef1b0319d47f1e2047edb500bbff5d06c808
Asif Raza <90905808+asifraza03@users.noreply.github.com>
2023-08-10 13:01:26 +0530

    Create prob(26).cpp

---
+++ prob(26).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void dhoom(vector<int> &v)
+{
+    for(int i=0;i<v.size();i++)
+    {
+        if(v[i+1])
+        {
+            v[i+1]=v[i]+v[i+1];
+        }
+    }
+    return;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    vector <int>v;
+    for(int i=0;i<n;i++)
+    {
+        int elem;
+        cin>>elem;
+        v.push_back(elem);
+    }
+
+    dhoom(v);
+    for(int i=0;i<n;i++)
+    {
+        cout<<v[i]<<" ";
+    }
+
+
+    return 0;
+}
