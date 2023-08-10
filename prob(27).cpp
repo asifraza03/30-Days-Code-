d50b75cf670052bfa532c351e945f4ddc79e4688
Asif Raza <90905808+asifraza03@users.noreply.github.com>
2023-08-10 13:44:21 +0530

    Create prob(27).cpp

---
+++ prob(27).cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < n; i++) {
+        int ele;
+        cin >> ele;
+        v.push_back(ele);
+    }
+    
+    int l, r;
+    cin >> l >> r;
+    int count = 0;
+
+    // Change the loop condition and indexing
+    for (int i = l; i <= r; i++) {
+        count += v[i];
+    }
+    
+    cout << count << endl;
+    return 0;
+}
