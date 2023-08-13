969ed5880f1298a037f18398fb54cd27eb787c61
Asif Raza <90905808+asifraza03@users.noreply.github.com>
2023-08-13 10:09:52 +0530

    Create prob(32).cpp

---
+++ prob(32).cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int rawnum(vector< vector <int > > &v)
+{
+    int maxone = INT_MIN;
+    int col = v[0].size();
+    int temp=0;
+    int chidya=-1;
+    for(int i=0;i<v.size();i++)
+    {
+        for(int j=0;j<v[i].size();j++)
+        {
+            if(v[i][j] == 1)
+            {
+                temp = col - j;
+                if( temp > maxone )
+                {
+                    maxone = temp;
+                    chidya = i;
+                }
+                break;
+            }
+
+        }
+    }
+    return chidya;
+}
+ 
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    vector< vector<int> >v(n,vector<int>(m));
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>v[i][j];
+        }
+    }
+
+    int dhoom = rawnum(v);
+    cout<<dhoom<<endl;
+    return 0;
+
+}
